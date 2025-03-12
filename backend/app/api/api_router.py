dea1e9b0fe0d69635f55b1e4b4bc1f898f3ff0c2
Krzysztof Wawrzkiewicz <krzysztof.wawrzkiewicz@fenergo.com>
2025-03-12 23:54:05 +0100

    working base backend

--- backend/app/api/api_router.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-from app.api.endpoints import items_router
-
-router = APIRouter()
-router.include_router(items_router, prefix="/items", tags=["items"])
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,30 +1,33 @@
+import sentry_sdk
 from fastapi import FastAPI
-from app.core.user_manager import fastapi_users, auth_backend
-from app.schemas.user import UserRead, UserCreate, UserUpdate
-from app.api.api_router import router as api_router
+from fastapi.routing import APIRoute
+from starlette.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from app.api.main import api_router
+from app.core.config import settings
 
-app.include_router(
-    fastapi_users.get_auth_router(auth_backend),
-    prefix="/auth/jwt",
-    tags=["auth"]
+
+def custom_generate_unique_id(route: APIRoute) -> str:
+    return f"{route.tags[0]}-{route.name}"
+
+
+if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
+    sentry_sdk.init(dsn=str(settings.SENTRY_DSN), enable_tracing=True)
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    generate_unique_id_function=custom_generate_unique_id,
 )
 
-app.include_router(
-    fastapi_users.get_register_router(UserRead, UserCreate),
-    prefix="/auth",
-    tags=["auth"]
-)
+# Set all CORS enabled origins
+if settings.all_cors_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
-app.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate),
-    prefix="/users",
-    tags=["users"]
-)
-
-app.include_router(api_router)
-
-@app.get("/")
-def root():
-    return {"message": "Welcome to the Onboarding API"}
+app.include_router(api_router, prefix=settings.API_V1_STR)
